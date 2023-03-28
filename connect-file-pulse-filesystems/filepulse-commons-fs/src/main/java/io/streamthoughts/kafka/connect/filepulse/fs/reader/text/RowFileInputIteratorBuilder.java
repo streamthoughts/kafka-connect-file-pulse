3771d2295f8cd49242db527d00a57e22361767db
Florian Hussonnois <florian.hussonnois@gmail.com>
2023-03-28 22:27:22 +0200

    fix(filesystems): fix checkstyle

--- connect-file-pulse-filesystems/filepulse-commons-fs/src/main/java/io/streamthoughts/kafka/connect/filepulse/fs/reader/text/RowFileInputIteratorBuilder.java
+++ connect-file-pulse-filesystems/filepulse-commons-fs/src/main/java/io/streamthoughts/kafka/connect/filepulse/fs/reader/text/RowFileInputIteratorBuilder.java
@@ -26,7 +26,6 @@ import io.streamthoughts.kafka.connect.filepulse.source.FileRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.function.Supplier;

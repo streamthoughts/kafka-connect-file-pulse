fe63ef37ce3a33a61161c985ec0f3c3783054100
TheWorkshopCom <dev@theworkshop.com>
2023-08-23 16:51:38 +0100

    feat(plugin): add built-in DateInFilenameFileListFilter

---
+++ connect-file-pulse-plugin/src/main/java/io/streamthoughts/kafka/connect/filepulse/fs/filter/DateInFilenameFileListFilter.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2023 StreamThoughts.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamthoughts.kafka.connect.filepulse.fs.filter;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+import io.streamthoughts.kafka.connect.filepulse.fs.PredicateFileListFilter;
+import io.streamthoughts.kafka.connect.filepulse.source.FileObjectMeta;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DateInFilenameFileListFilter extends PredicateFileListFilter {
+
+    private final static String GROUP = "DateInFilenameFileListFilter";
+
+    private static final Logger LOG = LoggerFactory.getLogger(DateInFilenameFileListFilter.class);
+
+    static final String FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG =
+            "file.filter.date.regex.extractor.pattern";
+
+    private static final String FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_DOC =
+            "The regex pattern used to extract the date from the filename";
+
+    static final String FILE_FILTER_DATE_FORMATTER_PATTERN_CONFIG =
+            "file.filter.date.formatter.pattern";
+
+    private static final String FILE_FILTER_DATE_FORMATTER_PATTERN_DOC =
+            "The formatter pattern used to format the date extracted from the filename";
+
+    static final String FILE_FILTER_DATE_MIN_DATE_CONFIG =
+            "file.filter.date.min.date";
+
+    private static final String FILE_FILTER_DATE_MIN_DATE_DOC =
+            "The minimum date that the date extracted from the filename should match (filenameDate >= minDate)";
+
+    static final String FILE_FILTER_DATE_MAX_DATE_CONFIG =
+            "file.filter.date.max.date";
+
+    private static final String FILE_FILTER_DATE_MAX_DATE_DOC =
+            "The maximum date that the date extracted from the filename should match (filenameDate < maxDate)";
+
+    private Pattern pattern;
+
+    DateTimeFormatter dateFormatter;
+
+    LocalDate minDate;
+
+    LocalDate maxDate;
+
+    @Override
+    public void configure(Map<String, ?> config) {
+        AbstractConfig abstractConfig = new AbstractConfig(getConfigDef(), config, false);
+        String pattern = abstractConfig.getString(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG);
+        if (pattern == null) {
+            throw new ConfigException("missing configuration: " + FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG);
+        }
+
+        setPattern(pattern);
+
+        dateFormatter = ofPattern(abstractConfig.getString(FILE_FILTER_DATE_FORMATTER_PATTERN_CONFIG));
+
+        String strMinDate = abstractConfig.getString(FILE_FILTER_DATE_MIN_DATE_CONFIG);
+        String strMaxDate = abstractConfig.getString(FILE_FILTER_DATE_MAX_DATE_CONFIG);
+
+        if (strMinDate == null && strMaxDate == null) {
+            throw new ConfigException("At least one of " + FILE_FILTER_DATE_MIN_DATE_CONFIG + " or " +
+                    FILE_FILTER_DATE_MAX_DATE_CONFIG + " should be specified");
+        }
+
+        if (strMinDate != null) {
+            minDate = DateValidator.parseDate(FILE_FILTER_DATE_MIN_DATE_CONFIG, strMinDate);
+        }
+        if (strMaxDate != null) {
+            maxDate = DateValidator.parseDate(FILE_FILTER_DATE_MAX_DATE_CONFIG, strMaxDate);
+        }
+    }
+
+    private static ConfigDef getConfigDef() {
+        DateValidator dateValidator = new DateValidator();
+        int groupCounter = 0;
+        return new ConfigDef()
+                .define(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG,
+                        ConfigDef.Type.STRING,
+                        ConfigDef.Importance.HIGH,
+                        FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_DOC,
+                        GROUP,
+                        groupCounter++,
+                        ConfigDef.Width.NONE,
+                        FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG)
+                .define(FILE_FILTER_DATE_FORMATTER_PATTERN_CONFIG,
+                        ConfigDef.Type.STRING,
+                        "yyyy-MM-dd",
+                        ConfigDef.Importance.HIGH,
+                        FILE_FILTER_DATE_FORMATTER_PATTERN_DOC,
+                        GROUP,
+                        groupCounter++,
+                        ConfigDef.Width.NONE,
+                        FILE_FILTER_DATE_FORMATTER_PATTERN_CONFIG)
+                .define(FILE_FILTER_DATE_MIN_DATE_CONFIG,
+                        ConfigDef.Type.STRING,
+                        null,
+                        dateValidator,
+                        ConfigDef.Importance.LOW,
+                        FILE_FILTER_DATE_MIN_DATE_DOC,
+                        GROUP,
+                        groupCounter++,
+                        ConfigDef.Width.NONE,
+                        FILE_FILTER_DATE_MIN_DATE_CONFIG)
+                .define(FILE_FILTER_DATE_MAX_DATE_CONFIG,
+                        ConfigDef.Type.STRING,
+                        null,
+                        dateValidator,
+                        ConfigDef.Importance.LOW,
+                        FILE_FILTER_DATE_MAX_DATE_DOC,
+                        GROUP,
+                        groupCounter,
+                        ConfigDef.Width.NONE,
+                        FILE_FILTER_DATE_MAX_DATE_CONFIG)
+                ;
+    }
+
+    @Override
+    public boolean test(FileObjectMeta meta) {
+        if (meta == null) {
+            return false;
+        }
+
+        Matcher matcher = pattern.matcher(meta.stringURI());
+        boolean matched = false;
+
+        if (matcher.find() && matcher.groupCount() > 0) {
+            matched = true;
+
+            String strDate = matcher.group(1);
+            LocalDate filenameDate = LocalDate.parse(strDate, dateFormatter);
+
+            if (minDate != null) {
+                matched = !filenameDate.isBefore(minDate);
+            }
+            if (maxDate != null) {
+                matched &= filenameDate.isBefore(maxDate);
+            }
+
+            if (!matched) {
+                String msg = String.format("Date '%s' is not between boundaries [%s, %s)",
+                        filenameDate, minDate == null ? "-inf" : minDate, maxDate == null ? "+inf" : maxDate);
+                LOG.debug(msg);
+            }
+        } else {
+            LOG.debug("Cannot extract date from '" + meta.stringURI() + "' using regexp '" + pattern.toString() + "'");
+        }
+
+        return matched;
+    }
+
+    private void setPattern(String pattern) {
+        this.pattern = Pattern.compile(pattern);
+    }
+
+    public static class DateValidator implements ConfigDef.Validator {
+
+        @Override
+        public void ensureValid(String name, Object value) {
+            String date = (String)value;
+            if (date != null) {
+                if (date.isEmpty()) {
+                    throw new ConfigException(name, value, "Date must be non-empty");
+                }
+
+                parseDate(name, date);
+            }
+        }
+
+        public static LocalDate parseDate(String configKey, String date) {
+            try {
+                return LocalDate.parse(date, ISO_LOCAL_DATE);
+            } catch (DateTimeParseException e) {
+                throw new ConfigException(configKey, date, "Cannot parse date: " + date);
+            }
+        }
+    }
+}
---
+++ connect-file-pulse-plugin/src/test/java/io/streamthoughts/kafka/connect/filepulse/fs/filter/DateInFilenameFileListFilterTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2023 StreamThoughts.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamthoughts.kafka.connect.filepulse.fs.filter;
+
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilter.FILE_FILTER_DATE_MAX_DATE_CONFIG;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilter.FILE_FILTER_DATE_MIN_DATE_CONFIG;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilter.FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.dateExtractorRegex;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.fileBetweenCutoffDates;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.fileDateAfterMaxCutoffDate;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.fileDateBeforeMinCutoffDate;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.fileDateEqualsMaxCutoffDate;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.fileDateEqualsMinCutoffDate;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.fileWithoutDateInName;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.invalidCutoffDate;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.maxCutoffDate;
+import static io.streamthoughts.kafka.connect.filepulse.fs.filter.DateInFilenameFileListFilterTest.Fixture.minCutoffDate;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import io.streamthoughts.kafka.connect.filepulse.source.FileObjectMeta;
+import io.streamthoughts.kafka.connect.filepulse.source.GenericFileObjectMeta;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.apache.kafka.common.config.ConfigException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class DateInFilenameFileListFilterTest {
+
+    @Test
+    void when_pattern_empty_configure_should_throw_exception() {
+        DateInFilenameFileListFilter filter = new DateInFilenameFileListFilter();
+        assertThatThrownBy(() -> filter.configure(Map.of()))
+                .isInstanceOf(ConfigException.class)
+                .hasMessageContaining(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG)
+                .hasNoCause();
+    }
+
+    @Test
+    void when_date_min_invalid_configure_should_throw_exception() {
+        Map<String, Object> configs =
+                Map.of(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG, dateExtractorRegex,
+                        FILE_FILTER_DATE_MIN_DATE_CONFIG, invalidCutoffDate);
+
+        DateInFilenameFileListFilter filter = new DateInFilenameFileListFilter();
+        assertThatThrownBy(() -> filter.configure(configs))
+                .isInstanceOf(ConfigException.class)
+                .hasMessageContaining(FILE_FILTER_DATE_MIN_DATE_CONFIG)
+                .hasNoCause();
+    }
+
+    @Test
+    void when_date_max_invalid_configure_should_throw_exception() {
+        Map<String, Object> configs =
+                Map.of(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG, dateExtractorRegex,
+                        FILE_FILTER_DATE_MAX_DATE_CONFIG, invalidCutoffDate);
+
+        DateInFilenameFileListFilter filter = new DateInFilenameFileListFilter();
+        assertThatThrownBy(() -> filter.configure(configs))
+                .isInstanceOf(ConfigException.class)
+                .hasMessageContaining(FILE_FILTER_DATE_MAX_DATE_CONFIG)
+                .hasNoCause();
+    }
+
+    @Test
+    void when_neither_max_date_nor_min_date_provided_configure_should_throw_exception() {
+        Map<String, Object> configs =
+                Map.of(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG, dateExtractorRegex);
+
+        DateInFilenameFileListFilter filter = new DateInFilenameFileListFilter();
+        assertThatThrownBy(() -> filter.configure(configs))
+                .isInstanceOf(ConfigException.class)
+                .hasMessageContaining("At least one of")
+                .hasNoCause();
+    }
+
+    private DateInFilenameFileListFilter prepareFilter(String minCutoffDate, String maxCutoffDate) {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(FILE_FILTER_DATE_REGEX_EXTRACTOR_PATTERN_CONFIG, dateExtractorRegex);
+
+        if (minCutoffDate != null) {
+            configs.put(FILE_FILTER_DATE_MIN_DATE_CONFIG, minCutoffDate);
+        }
+
+        if (maxCutoffDate != null) {
+            configs.put(FILE_FILTER_DATE_MAX_DATE_CONFIG, maxCutoffDate);
+        }
+
+        DateInFilenameFileListFilter filter = new DateInFilenameFileListFilter();
+        filter.configure(configs);
+
+        return filter;
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void when_called_test_should_return_expected_value(Boolean expected,
+                                                       FileObjectMeta meta,
+                                                       String minCutoffDate,
+                                                       String maxCutoffDate) {
+
+        DateInFilenameFileListFilter filter = prepareFilter(minCutoffDate, maxCutoffDate);
+        assertThat(filter.test(meta)).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> when_called_test_should_return_expected_value() {
+        return Stream.of(
+                arguments(false, fileWithoutDateInName, minCutoffDate, null),
+                arguments(false, fileWithoutDateInName, null, maxCutoffDate),
+                arguments(false, fileWithoutDateInName, minCutoffDate, maxCutoffDate),
+                arguments(true,  fileDateEqualsMinCutoffDate, minCutoffDate, null),
+                arguments(true,  fileDateEqualsMinCutoffDate, minCutoffDate, maxCutoffDate),
+                arguments(false, fileDateBeforeMinCutoffDate, minCutoffDate, null),
+                arguments(false, fileDateBeforeMinCutoffDate, minCutoffDate, maxCutoffDate),
+                arguments(true,  fileBetweenCutoffDates, minCutoffDate, maxCutoffDate),
+                arguments(false, fileDateEqualsMaxCutoffDate, null, maxCutoffDate),
+                arguments(false, fileDateEqualsMaxCutoffDate, minCutoffDate, maxCutoffDate),
+                arguments(false, fileDateAfterMaxCutoffDate, null, maxCutoffDate),
+                arguments(false, fileDateAfterMaxCutoffDate, minCutoffDate, maxCutoffDate)
+        );
+    }
+
+    interface Fixture {
+        String minCutoffDate = "2022-04-01";
+        String maxCutoffDate = "2022-12-31";
+        String invalidCutoffDate = "2022-04-41";
+        String dateExtractorRegex = "^.*_(\\d{4}-\\d{2}-\\d{2}).*$";
+
+        FileObjectMeta fileWithoutDateInName = new GenericFileObjectMeta.Builder()
+                .withName("f1.txt")
+                .withUri(URI.create("/path/f1.txt"))
+                .withContentLength(1024L)
+                .withLastModified(System.currentTimeMillis())
+                .build();
+
+        FileObjectMeta fileDateEqualsMinCutoffDate = new GenericFileObjectMeta.Builder()
+                .withName("f1_2022-04-01.txt")
+                .withUri(URI.create("/path/f1_2022-04-01.txt"))
+                .withContentLength(1024L)
+                .withLastModified(System.currentTimeMillis())
+                .build();
+        FileObjectMeta fileDateBeforeMinCutoffDate = new GenericFileObjectMeta.Builder()
+                .withName("f1_2022-03-01.txt")
+                .withUri(URI.create("/path/f1_2022-03-01.txt"))
+                .withContentLength(1024L)
+                .withLastModified(System.currentTimeMillis())
+                .build();
+        FileObjectMeta fileDateAfterMaxCutoffDate = new GenericFileObjectMeta.Builder()
+                .withName("f1_2023-01-01.txt")
+                .withUri(URI.create("/path/f1_2023-01-01.txt"))
+                .withContentLength(1024L)
+                .withLastModified(System.currentTimeMillis())
+                .build();
+
+        FileObjectMeta fileDateEqualsMaxCutoffDate = new GenericFileObjectMeta.Builder()
+                .withName("f1_2022-12-31.txt")
+                .withUri(URI.create("/path/f1_2022-12-31.txt"))
+                .withContentLength(1024L)
+                .withLastModified(System.currentTimeMillis())
+                .build();
+
+        FileObjectMeta fileBetweenCutoffDates = new GenericFileObjectMeta.Builder()
+                .withName("f1_2022-11-31.txt")
+                .withUri(URI.create("/path/f1_2022-11-31.txt"))
+                .withContentLength(1024L)
+                .withLastModified(System.currentTimeMillis())
+                .build();
+    }
+}
